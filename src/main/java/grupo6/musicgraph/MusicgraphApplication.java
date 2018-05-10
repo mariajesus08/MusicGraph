7ff3df0cdfcdf64557b0e794a51973ab481f4850
María Jesús <mjcamposandrade@gmail.com>
2018-05-10 16:01:09 -0300

    Commit inicial

---
+++ src/main/java/grupo6/musicgraph/MusicgraphApplication.java
@@ -0,0 +1,12 @@
+package grupo6.musicgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicgraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicgraphApplication.class, args);
+    }
+}
---
+++ src/test/java/grupo6/musicgraph/MusicgraphApplicationTests.java
@@ -0,0 +1,16 @@
+package grupo6.musicgraph;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicgraphApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
