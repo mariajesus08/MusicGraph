d898951a2d8e273d7139d9fd7801e05e96aa033e
María Jesús <mjcamposandrade@gmail.com>
2018-07-13 23:24:55 -0400

    actualización gitignore

--- restApi/src/main/java/tbd/restapi/repositories/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package tbd.restapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tbd.restapi.models.Genre;
-
-public interface GenreRepository extends JpaRepository<Genre,Integer> {
-    public Genre findGenreById(Integer id);
-}
--- restApi/src/main/java/tbd/restapi/repositories/StatisticRepository.java
+++
@@ -1,8 +0,0 @@
-package tbd.restapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tbd.restapi.models.Statistic;
-
-public interface StatisticRepository extends JpaRepository<Statistic,Integer> {
-    public Statistic findStatisticById(Integer id);
-}
