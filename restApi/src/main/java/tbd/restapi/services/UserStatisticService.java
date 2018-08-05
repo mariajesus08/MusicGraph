ba6c52230125577713ca3e49d6a7e54d4e5a2675
GarsePersen <sebastian.garay.p@usach.cl>
2018-08-05 03:23:29 -0400

    Se arregla userStatistics

--- restApi/src/main/java/tbd/restapi/services/UserStatisticService.java
+++ restApi/src/main/java/tbd/restapi/services/UserStatisticService.java
@@ -22,6 +22,7 @@ public class UserStatisticService {
     private UserStatisticRepository UserStatisticRepository ;
 
    
+    
     @CrossOrigin
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
