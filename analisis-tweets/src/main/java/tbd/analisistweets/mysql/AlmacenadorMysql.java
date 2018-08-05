396eefc2dad2aca4534152ef9b6a5671b8e8d847
GarsePersen <sebastian.garay.p@usach.cl>
2018-08-05 13:32:35 -0400

    Se arregla append de mysql

--- analisis-tweets/src/main/java/tbd/analisistweets/mysql/AlmacenadorMysql.java
+++ analisis-tweets/src/main/java/tbd/analisistweets/mysql/AlmacenadorMysql.java
@@ -3,7 +3,9 @@ package tbd.analisistweets.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AlmacenadorMysql {
     public void insertarEstadistica(String artista, Float positivos, Float negativos, Integer totales){
@@ -26,13 +28,31 @@ public class AlmacenadorMysql {
         String query = "INSERT INTO statistics (name,negative_tweets,positive_tweets, total_tweets) VALUES (?,?,?,?)";
         String setIds =  "UPDATE statistics, artists SET statistics.id_artist = artists.id WHERE artists.name = statistics.name;";
         String setGenres =  "UPDATE statistics, artists SET statistics.id_genre = artists.id_genre WHERE artists.id = statistics.id_artist;";
-
+        String queryGet = "SELECT * FROM statistics WHERE statistics.name = \""+artista+"\";";
+        System.out.println(queryGet);
         try{
+
             PreparedStatement ps = connection.prepareStatement(query);
             PreparedStatement ps2 = connection.prepareStatement(setIds);
             PreparedStatement ps3 = connection.prepareStatement(setGenres);
             
-            
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(queryGet);
+            int total_tweetsAux = 0;
+            float positivosAux = 0f;
+            float negativosAux = 0f;
+            while(rs.next()){
+                total_tweetsAux = rs.getInt("total_tweets");
+                positivosAux = rs.getInt("positive_tweets");
+                negativosAux = rs.getInt("negative_tweets");
+            }
+            totales = totales + total_tweetsAux;
+            if(positivosAux != 0f){
+                positivos = (positivos+positivosAux)/2;
+            }
+            if(negativosAux != 0f){
+                negativos = (negativos+negativosAux)/2;
+            }
             ps.setString(1, artista);
             ps.setFloat(2, negativos);
             ps.setFloat(3, positivos);
