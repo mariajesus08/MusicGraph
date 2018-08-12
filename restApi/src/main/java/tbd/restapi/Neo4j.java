4fc70da3ab45705802adc86091a5ed223a076a2d
María Jesús <mjcamposandrade@gmail.com>
2018-08-12 20:10:06 -0300

    Consultas actuales neo4j

--- restApi/src/main/java/tbd/restapi/Neo4j.java
+++ restApi/src/main/java/tbd/restapi/Neo4j.java
@@ -2,8 +2,11 @@ package tbd.restapi;
 
 import org.neo4j.driver.v1.*;
 import org.neo4j.driver.v1.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import tbd.restapi.models.Artist;
+import tbd.restapi.models.Common_User;
 import tbd.restapi.models.UserStatistic;
+import tbd.restapi.repositories.*;
 
 import java.sql.*;
 import java.sql.Statement;
@@ -13,88 +16,52 @@ import java.util.List;
 
 public class Neo4j {
 
-    public void crearGrafo(){
-        Connection connection = null;
-        String username = "root";
-        String password = "secret1234";
-        String host = "jdbc:mysql://165.227.12.119:3306/";
-        String db_name = "musicgraphdb?useSSL=false";
-        List<Artist> artists = new ArrayList<>();
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(host + db_name, username, password);
-            String query = "SELECT name FROM artists";
-            Statement st = connection.createStatement();
-            ResultSet resultset = st.executeQuery(query);
-            while (resultset.next()) {
-                Artist artist = new Artist();
-                String nombre = resultset.getString("name");
-                nombre = nombre.replace("'","");
-                artist.setName(nombre);
-                artists.add(artist);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    private CommonUserRepository commonUserRepository;
 
-        List<UserStatistic> userStatistics = new ArrayList<>();
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(host + db_name, username, password);
-            String query = "SELECT name, retweets, followers, lastTweet, date, artist FROM userStatistics";
-            Statement st = connection.createStatement();
-            ResultSet resultset = st.executeQuery(query);
-            while (resultset.next()) {
-                UserStatistic userStatistic = new UserStatistic();
-                userStatistic.setName(resultset.getString("name"));
-                userStatistic.setRetweets(resultset.getInt("retweets"));
-                userStatistic.setFollowers(resultset.getInt("followers"));
-                userStatistic.setLast_tweet(resultset.getString("lastTweet"));
-                userStatistic.setDate(resultset.getDate("date"));
-                userStatistic.setArtist(resultset.getString("artist"));
-                userStatistics.add(userStatistic);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    private InfluyentUserRepository influyentUserRepository;
+
+    @Autowired
+    private CommonUserArtistRepository commonUserArtistRepository;
+
+    @Autowired
+    private InfluyentUserArtistRepository influyentUserArtistRepository;
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    public void crearGrafo(){
 
         Driver driver = GraphDatabase.driver("bolt://165.227.12.119:7687", AuthTokens.basic("neo4j", "root123"));
         Session session = driver.session();
         session.run("match (a)-[r]->(b) delete r");
         session.run("match (n) delete n");
-        for(UserStatistic userStatistic : userStatistics){
-            session.run("CREATE (u:User {name: \""+ userStatistic.getName() + "\""
-                    + ", retweets:\""+ userStatistic.getRetweets() +"\""
-                    + ", followers:\""+ userStatistic.getFollowers() +"\""
-                    + ", last_tweet:\""+ userStatistic.geLast_tweet() +"\""
-                    + ", verified:\""+ userStatistic.getVerified() +"\""
-                    + ", relevant:\""+ userStatistic.getRelevant() +"\""
-                    + ", date:\""+ userStatistic.getDate() +"\""
-                    + ", artist:\""+ userStatistic.getArtist() +"\""
+        List<Common_User> common_users = commonUserRepository.findAll();
+        //PARA AGREGAR LOS NODOS
+        for(Common_User common_user : common_users){
+            System.out.println(common_user);
+            session.run("CREATE (u:User {name: \""+ common_user.getName() + "\""
+                    + ", followers:\""+ common_user.getFollowers() +"\""
                     + "})");
         }
-
+        List<Artist> artists = artistRepository.findAll();
+        //PARA agregar Artistas
         for(Artist artist :artists){
             session.run("CREATE (a:Artist {name:'" + artist.getName() + "'"
                      +
                     "})");
         }
 
-        for(UserStatistic userStatistic : userStatistics) {
+   /*     for(UserStatistic userStatistic : userStatistics) {
             String nombreUsuario = userStatistic.getName();
             String nombreArtista = userStatistic.getArtist();
             nombreArtista = nombreArtista.replace("'", "");
+            //PARA AGREGAR LAS RELACIONES
             session.run("match (u:User) where u.name='"+ nombreUsuario+ "' "
                     + "  match (a:Artist) where a.name='" + nombreArtista + "' "
                     + "  create (u)-[r:comenta]->(a)");
-        }
-
-        StatementResult result = session.run( "MATCH (n:Artist) RETURN n");
-        while ( result.hasNext() )
-        {
-            Record record = result.next();
-            System.out.println( record.get("name") );
-        }
+        }*/
         session.close();
         driver.close();
     }
